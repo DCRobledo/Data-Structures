6c6fe4ef0dea58d231bdc70a4a2a7c6c83c683b8
DCRobledo <daniel.canorobledo@gmail.com>
2020-12-03 17:09:07 +0100

    ArrayList Implemented

--- src/main/java/DS/ArrayList/ArrayList.java
+++ src/main/java/DS/ArrayList/ArrayList.java
@@ -1,5 +1,38 @@
 package DS.ArrayList;
 
 public class ArrayList {
-    
+    private static final int SIZE_FACTOR = 5;
+
+    private Object data[];
+
+    private int index;
+    private int size;
+
+    public ArrayList(){
+        this.data = new Object[SIZE_FACTOR];
+        this.size = SIZE_FACTOR;
+    }
+
+    public void add (Object obj){
+        if(this.index == this.size-1) increaseSizeAndReallocate();
+        data[index] = obj;
+        this.index++;
+    }
+
+    private void increaseSizeAndReallocate() {
+        this.size += SIZE_FACTOR;
+        
+        Object newData[] = new Object[size];
+        for(int i = 0; i<data.length; i++) newData[i] = this.data[i];
+        this.data = newData;
+    }
+
+    public Object get(int i){
+        return this.data[i];
+    }
+
+    public void remove(int i){
+        for(int x = 0; x<data.length-1; x++) data[x] = data[x+1];
+        this.index--;
+    }
 }
