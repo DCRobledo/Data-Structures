1f6cf394af0d6ca59d807202f66a278c4c4d7f83
DCRobledo <daniel.canorobledo@gmail.com>
2020-12-05 11:46:00 +0100

    Graph (List of LinkedLists) Implemented

--- src/main/java/DS/Graph.java
+++ src/main/java/DS/Graph.java
@@ -1,4 +1,26 @@
 package DS;
+
+import java.util.LinkedList;
+import java.util.List;
+
 public class Graph {
-    
+    private class Edge {
+        private int vertexOne, vertexTwo;
+        private Edge(int vertexOne, int vertexTwo) {
+            this.vertexOne = vertexOne;
+            this.vertexTwo = vertexTwo;
+        }
+    }
+
+    public List<Edge> edges[];
+
+    public Graph (int numVertex) {
+        edges = new LinkedList[numVertex];
+        for(int i = 0; i<edges.length; i++)
+            edges[i] = new LinkedList<Edge>();
+    }
+
+    public void addEdge(int edge, int vertexOne, int vertexTwo) {
+        edges[edge].add(new Edge(vertexOne, vertexTwo));
+    }
 }
