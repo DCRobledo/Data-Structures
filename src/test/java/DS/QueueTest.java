6f2dc4f255d36ab040466d55ed66ead5a852caa9
DCRobledo <daniel.canorobledo@gmail.com>
2020-12-07 11:43:44 +0100

    Queue Tests Implemented. PASS

--- src/test/java/DS/QueueTest.java
+++ src/test/java/DS/QueueTest.java
@@ -24,5 +24,22 @@ public class QueueTest {
         q.add(3);
         assertEquals(3, q.peek());
     }
+
+    @Test
+    public void addAndRemoveTest() {
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        
+        boolean firstRemove = q.remove() == 1;
+        boolean secondRemove = q.remove() == 2;  
+        boolean thirdRemove = q.remove() == 3;
+        
+        q.add(1);
+
+        boolean isEmpty = q.isEmpty();
+
+        assertTrue(!isEmpty && firstRemove && secondRemove && thirdRemove);
+    }
 }
     
