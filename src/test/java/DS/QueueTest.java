52a024fcf28d869254368905e12fe25f34af8d68
DCRobledo <daniel.canorobledo@gmail.com>
2020-12-07 11:34:40 +0100

    JUnit in VSC Testing

--- src/test/java/DS/QueueTest.java
+++ src/test/java/DS/QueueTest.java
@@ -1,17 +1,28 @@
 package DS;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Before;    
     
 public class QueueTest {
+    protected Queue q;
 
     @Before
     public void setup(){
-
+        q = new Queue();
     }
         
     @Test
-    public void test() {
-        
+    public void isEmptyTest() {
+        assertTrue(q.isEmpty());
+    }
+
+    @Test
+    public void peekTest() {
+        q.add(3);
+        assertEquals(3, q.peek());
     }
 }
     
